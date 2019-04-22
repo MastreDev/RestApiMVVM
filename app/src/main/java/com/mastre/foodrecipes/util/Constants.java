d3eed546e4641fe7ac5a276792d85917b417b6a8
peaceyop <peaceyop@gmail.com>
2019-04-22 18:04:36 +0900

    Add Food2Fork API Strings

---
+++ app/src/main/java/com/mastre/foodrecipes/util/Constants.java
@@ -0,0 +1,8 @@
+package com.mastre.foodrecipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.food2fork.com";
+
+    public static final String API_KEY_0 = "a66fb1bcf057aba38cad5c4f4c800957";
+}
